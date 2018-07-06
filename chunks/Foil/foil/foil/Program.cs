ecc5a8ef007ff00596f5cda00201fb8dc2e1eb95
TheAlchemist404 <37026488+TheAlchemist404@users.noreply.github.com>
2018-07-06 13:57:29 -0700

    Add files via upload

---
+++ chunks/Foil/foil/foil/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace foil
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,,] finalM;
+            string dir = @"C:\Users\Catedras05\Desktop\TestTable.csv";
+            string raw;
+            using (var reader = new StreamReader(dir))
+            {
+                raw = reader.ReadToEnd();
+            }
+            //get the info for the tests as big Strings
+            String[] TestsRaw = Regex.Split(raw, "\r\n,,\r\n");
+
+            // NOTE: the last chunk will have a empty line this is to delete the line before proceeding
+            TestsRaw[TestsRaw.Length - 1] = TestsRaw[TestsRaw.Length - 1].Remove(TestsRaw[TestsRaw.Length - 1].Length - 2);
+
+            //get the coordinates as Strings as "x,y,time"
+            var Coords = new List<String[]>();
+            foreach (var Set in TestsRaw)
+            {
+                var V = Set;
+                if (Set.Contains(",,")) { V = V.Substring(V.IndexOf(",,")+4); }
+                Coords.Add(Regex.Split(Set, "\r\n"));   
+            }
+        }
+    }
+}
---
+++ chunks/readerTest/readerTest/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using System.IO;
+
+namespace readerTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dir = @"C:\Users\Catedras05\Desktop\TestTable.csv";
+            string raw;
+            using (var reader = new StreamReader(dir))
+            {
+                raw = reader.ReadToEnd();
+            }
+            Console.WriteLine(returnNtest(raw).ToString());
+            String[] Nraw = Regex.Split(raw, "\r\n,,\r\n");
+            Nraw[Nraw.Length - 1] = Nraw[Nraw.Length - 1].Remove(Nraw[Nraw.Length - 1].Length - 2);
+            Console.WriteLine(returnNcoords(Nraw[2]).ToString());
+            Console.Read();
+
+            
+        }
+        public static int returnNtest(string rawTable)
+        {
+            int N=0;
+
+            N=Regex.Matches(rawTable, "\n,,\r").Count;
+
+            return N+1;
+        }
+        public static int returnNcoords(string rawSet)
+        {
+            int N = 0;
+            N= Regex.Matches(rawSet, "\n").Count;
+            if (rawSet.Contains(",,"))
+            {
+                N--;
+            }
+            return N+1;
+        }
+        
+    }
+}

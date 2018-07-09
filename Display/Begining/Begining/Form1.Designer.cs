eab2754fc181684d4f14eeffc29164fee8a552ef
TheAlchemist404 <37026488+TheAlchemist404@users.noreply.github.com>
2018-07-09 15:02:20 -0700

    Add files via upload

---
+++ Display/Begining/Begining/Form1.Designer.cs
@@ -0,0 +1,255 @@
+﻿namespace Begining
+{
+    partial class Test
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Test));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.Box1 = new System.Windows.Forms.PictureBox();
+            this.Box2 = new System.Windows.Forms.PictureBox();
+            this.Box3 = new System.Windows.Forms.PictureBox();
+            this.Box4 = new System.Windows.Forms.PictureBox();
+            this.Box5 = new System.Windows.Forms.PictureBox();
+            this.Box6 = new System.Windows.Forms.PictureBox();
+            this.Box7 = new System.Windows.Forms.PictureBox();
+            this.Box8 = new System.Windows.Forms.PictureBox();
+            this.Box9 = new System.Windows.Forms.PictureBox();
+            this.Box10 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.Box1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Box2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Box3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Box4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Box5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Box6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Box7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Box8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Box9)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Box10)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 5000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Box1
+            // 
+            this.Box1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Box1.BackgroundImage")));
+            this.Box1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Box1.Image = ((System.Drawing.Image)(resources.GetObject("Box1.Image")));
+            this.Box1.Location = new System.Drawing.Point(12, 75);
+            this.Box1.Name = "Box1";
+            this.Box1.Size = new System.Drawing.Size(10, 10);
+            this.Box1.TabIndex = 11;
+            this.Box1.TabStop = false;
+            this.Box1.Visible = false;
+            // 
+            // Box2
+            // 
+            this.Box2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Box2.BackgroundImage")));
+            this.Box2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Box2.Image = ((System.Drawing.Image)(resources.GetObject("Box2.Image")));
+            this.Box2.Location = new System.Drawing.Point(12, 91);
+            this.Box2.Name = "Box2";
+            this.Box2.Size = new System.Drawing.Size(10, 10);
+            this.Box2.TabIndex = 12;
+            this.Box2.TabStop = false;
+            this.Box2.Visible = false;
+            // 
+            // Box3
+            // 
+            this.Box3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Box3.BackgroundImage")));
+            this.Box3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Box3.Image = ((System.Drawing.Image)(resources.GetObject("Box3.Image")));
+            this.Box3.Location = new System.Drawing.Point(12, 107);
+            this.Box3.Name = "Box3";
+            this.Box3.Size = new System.Drawing.Size(10, 10);
+            this.Box3.TabIndex = 13;
+            this.Box3.TabStop = false;
+            this.Box3.Visible = false;
+            // 
+            // Box4
+            // 
+            this.Box4.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Box4.BackgroundImage")));
+            this.Box4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Box4.Image = ((System.Drawing.Image)(resources.GetObject("Box4.Image")));
+            this.Box4.Location = new System.Drawing.Point(12, 171);
+            this.Box4.Name = "Box4";
+            this.Box4.Size = new System.Drawing.Size(10, 10);
+            this.Box4.TabIndex = 14;
+            this.Box4.TabStop = false;
+            this.Box4.Visible = false;
+            // 
+            // Box5
+            // 
+            this.Box5.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Box5.BackgroundImage")));
+            this.Box5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Box5.Image = ((System.Drawing.Image)(resources.GetObject("Box5.Image")));
+            this.Box5.Location = new System.Drawing.Point(12, 123);
+            this.Box5.Name = "Box5";
+            this.Box5.Size = new System.Drawing.Size(10, 10);
+            this.Box5.TabIndex = 15;
+            this.Box5.TabStop = false;
+            this.Box5.Visible = false;
+            // 
+            // Box6
+            // 
+            this.Box6.BackgroundImage = global::Begining.Properties.Resources.dot;
+            this.Box6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Box6.Image = ((System.Drawing.Image)(resources.GetObject("Box6.Image")));
+            this.Box6.Location = new System.Drawing.Point(12, 176);
+            this.Box6.Name = "Box6";
+            this.Box6.Size = new System.Drawing.Size(10, 10);
+            this.Box6.TabIndex = 16;
+            this.Box6.TabStop = false;
+            this.Box6.Visible = false;
+            // 
+            // Box7
+            // 
+            this.Box7.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Box7.BackgroundImage")));
+            this.Box7.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Box7.Image = ((System.Drawing.Image)(resources.GetObject("Box7.Image")));
+            this.Box7.Location = new System.Drawing.Point(12, 139);
+            this.Box7.Name = "Box7";
+            this.Box7.Size = new System.Drawing.Size(10, 10);
+            this.Box7.TabIndex = 17;
+            this.Box7.TabStop = false;
+            this.Box7.Visible = false;
+            // 
+            // Box8
+            // 
+            this.Box8.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Box8.BackgroundImage")));
+            this.Box8.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Box8.Image = ((System.Drawing.Image)(resources.GetObject("Box8.Image")));
+            this.Box8.Location = new System.Drawing.Point(12, 155);
+            this.Box8.Name = "Box8";
+            this.Box8.Size = new System.Drawing.Size(10, 10);
+            this.Box8.TabIndex = 18;
+            this.Box8.TabStop = false;
+            this.Box8.Visible = false;
+            // 
+            // Box9
+            // 
+            this.Box9.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Box9.BackgroundImage")));
+            this.Box9.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Box9.Image = ((System.Drawing.Image)(resources.GetObject("Box9.Image")));
+            this.Box9.Location = new System.Drawing.Point(12, 187);
+            this.Box9.Name = "Box9";
+            this.Box9.Size = new System.Drawing.Size(10, 10);
+            this.Box9.TabIndex = 19;
+            this.Box9.TabStop = false;
+            this.Box9.Visible = false;
+            // 
+            // Box10
+            // 
+            this.Box10.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Box10.BackgroundImage")));
+            this.Box10.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Box10.Image = ((System.Drawing.Image)(resources.GetObject("Box10.Image")));
+            this.Box10.Location = new System.Drawing.Point(12, 197);
+            this.Box10.Name = "Box10";
+            this.Box10.Size = new System.Drawing.Size(10, 10);
+            this.Box10.TabIndex = 20;
+            this.Box10.TabStop = false;
+            this.Box10.Visible = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(115, 92);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 21;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "Open File";
+            this.openFileDialog1.Filter = "Archivo separado por comas(*.csv)|*.csv";
+            // 
+            // Test
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Silver;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(485, 430);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Box10);
+            this.Controls.Add(this.Box9);
+            this.Controls.Add(this.Box8);
+            this.Controls.Add(this.Box7);
+            this.Controls.Add(this.Box6);
+            this.Controls.Add(this.Box5);
+            this.Controls.Add(this.Box4);
+            this.Controls.Add(this.Box3);
+            this.Controls.Add(this.Box2);
+            this.Controls.Add(this.Box1);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Test";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Test_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.Box1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Box2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Box3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Box4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Box5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Box6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Box7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Box8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Box9)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Box10)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox Box1;
+        private System.Windows.Forms.PictureBox Box2;
+        private System.Windows.Forms.PictureBox Box3;
+        private System.Windows.Forms.PictureBox Box4;
+        private System.Windows.Forms.PictureBox Box5;
+        private System.Windows.Forms.PictureBox Box6;
+        private System.Windows.Forms.PictureBox Box7;
+        private System.Windows.Forms.PictureBox Box8;
+        private System.Windows.Forms.PictureBox Box9;
+        private System.Windows.Forms.PictureBox Box10;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ Display/Begining/Begining/Form1.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace Begining
+{
+    public partial class Test : Form //add diferent cuantities for the dots display
+    {
+        int cPoint = 0, cTest = 0;
+        PictureBox[] Dots = new PictureBox[10];
+        int[,,] Pdata;
+        string[] picsroute;
+
+
+        public Test()
+        {
+            InitializeComponent();
+            Dots[0] = Box1;
+            Dots[1] = Box2;
+            Dots[2] = Box3;
+            Dots[3] = Box4;
+            Dots[4] = Box5;
+            Dots[5] = Box6;
+            Dots[6] = Box7;
+            Dots[7] = Box8;
+            Dots[8] = Box9;
+            Dots[9] = Box10;
+            foreach (var item in Dots)
+            {
+                item.Height = this.Height / 40;
+                item.Width = item.Height;
+            }
+            openFileDialog1.ShowDialog();
+
+        }
+
+        private void Test_Load(object sender, EventArgs e)
+        {
+            var Stuff = getcoords(openFileDialog1.FileName);
+            Pdata = Stuff.Item1;
+            picsroute = Stuff.Item2;
+            var X = new List<int>();
+            var Y = new List<int>();
+            for (int i = 0; i < Pdata.GetLength(1); i++)
+            {
+                X.Add(Pdata[cTest, i, 0]);
+                Y.Add(Pdata[cTest, i, 1]);
+                //Dots.Add(InitializePictureBox());
+            }
+            SetPoints(X.ToArray(), Y.ToArray());
+            button1.Left = (this.Width / 2) - (button1.Width / 2);
+            button1.Top = (this.Height / 2) - (button1.Height / 2);
+        }
+
+        private PictureBox InitializePictureBox()
+        {
+            var PictureBox1 = new PictureBox();
+
+            // Set the location and size of the PictureBox control.
+            PictureBox1.Location = new System.Drawing.Point(0, 0);
+            PictureBox1.Size = new System.Drawing.Size(10, 10);
+            PictureBox1.TabStop = false;
+
+
+            // Set the SizeMode property to the StretchImage value.  This
+            // will shrink or enlarge the image as needed to fit into
+            // the PictureBox.
+            PictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            PictureBox1.BackgroundImage = Begining.Properties.Resources.dot;
+
+            // Set the border style to a three-dimensional border.
+            PictureBox1.BorderStyle = BorderStyle.None;
+            PictureBox1.Visible = false;
+            PictureBox1.Enabled = true;
+
+            // Add the PictureBox to the form.
+            this.Controls.Add(PictureBox1);
+
+            return PictureBox1;
+        }//edit this part to add more points
+
+        public Tuple<int[,,], string[]> getcoords(string FileDir)
+        {
+            Tuple < int[,,],string[]> R;
+            int[,,] finalM;
+            string[] pics;
+            //string dir = @"C:\Users\Lenovo\Desktop\TestTable.csv";
+            string raw;
+            try
+            {
+                using (var reader = new StreamReader(FileDir))
+                {
+                    raw = reader.ReadToEnd();
+                }
+                //get the info for the tests as big Strings
+                String[] TestsRaw = Regex.Split(raw, "\r\n,,\r\n");
+
+                // NOTE: the last chunk will have a empty line this is to delete the line before proceeding
+                TestsRaw[TestsRaw.Length - 1] = TestsRaw[TestsRaw.Length - 1].Remove(TestsRaw[TestsRaw.Length - 1].Length - 2);
+
+                //get the coordinates as Strings as "x,y,time"
+                var Coords = new List<String[]>();
+                foreach (var Set in TestsRaw)
+                {
+                    var V = Set;
+                    if (Set.Contains(",,")) { V = V.Substring(V.LastIndexOf(",,") + 4); }
+                    Coords.Add(Regex.Split(V, "\r\n"));
+                }
+
+                //get the max amount of points
+                int Max = 0;
+                foreach (var C in Coords)
+                {
+                    if (Max < C.Length)
+                    {
+                        Max = C.Length;
+                    }
+                }
+
+                finalM = new int[TestsRaw.Length, Max, 3];
+                pics = new string[TestsRaw.Length];
+                var Buff = new String[3];
+
+                for (int i = 0; i < TestsRaw.Length; i++)
+                {
+                    if (TestsRaw[i].Contains(",,")) { pics[i] = TestsRaw[i].Substring(0,TestsRaw[i].LastIndexOf(",,")-2); }
+                    for (int i2 = 0; i2 < Coords[i].Length; i2++)
+                    {
+                        Buff = Regex.Split(Coords[i][i2], ",");
+                        finalM[i, i2, 0] = Convert.ToInt32(Buff[0]);
+                        finalM[i, i2, 1] = Convert.ToInt32(Buff[1]);
+                        finalM[i, i2, 2] = Convert.ToInt32(Buff[2]);
+                    }
+                }
+                R = Tuple.Create(finalM, pics);
+                return R;
+            }
+            catch (FileNotFoundException)
+            {
+                Application.Exit();
+                return null;
+            }
+        }
+
+        public void wipePoints()
+        {
+            foreach (PictureBox item in Dots)
+            {
+                item.Visible = false;
+                //item.Left = 0;
+                //item.Top = 0;
+            }
+        }
+
+        public void SetPoints(int[] coordsX, int[] coordsY)
+        {
+            for (int i = 0; i < coordsX.Length; i++)
+            {
+                Dots[i].Left = (((this.Width - Dots[i].Width) / 100) * coordsX[i]) - (Dots[i].Width / 2);
+                Dots[i].Top = (((this.Height - Dots[i].Height) / 100) * coordsY[i])-(Dots[i].Height/2);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+            button1.Visible = false;
+            button1.Enabled = false;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (cPoint < Pdata.GetLength(1) && Pdata[cTest, cPoint, 2] > 0)
+            {
+                wipePoints();
+                Dots[cPoint].Visible = true;
+                timer1.Interval = Pdata[cTest, cPoint, 2]*1000;
+                cPoint++;
+            }
+            else
+            {
+                cTest++;
+                cPoint = 0;
+                timer1.Interval = 5000;
+                wipePoints();
+                try
+                {
+                    setBackground(cTest);
+                    var X= new List<int>();
+                    var Y= new List<int>();
+                    for (int i = 0; i < Pdata.GetLength(1); i++)
+                    {
+                        X.Add(Pdata[cTest, i, 0]);
+                        Y.Add(Pdata[cTest, i, 1]);
+                    }
+                    SetPoints(X.ToArray(), Y.ToArray());
+                }
+                catch (IndexOutOfRangeException)
+                {
+                    timer1.Enabled = false;
+                    cTest = 0;
+                    cPoint = 0;
+                    button1.Enabled = true;
+                    button1.Visible = true;
+                    this.BackgroundImage = null;
+                }
+            }
+        }
+
+        public void setBackground(int index)
+        {
+            //try
+            //{
+                this.BackgroundImage = Image.FromFile((openFileDialog1.FileName.Remove(openFileDialog1.FileName.Length - openFileDialog1.SafeFileName.Length))+picsroute[index]);
+            /*}
+            catch (FileNotFoundException)
+            {
+                this.BackgroundImage = null;
+            }*/
+            
+        }
+    }
+}

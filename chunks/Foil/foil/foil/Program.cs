9e04d5cb20996d71ab3fc10ccd45c82f7ff44d68
TheAlchemist404 <37026488+TheAlchemist404@users.noreply.github.com>
2018-07-08 19:25:01 -0700

    Update Program.cs

--- chunks/Foil/foil/foil/Program.cs
+++ chunks/Foil/foil/foil/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Text.RegularExpressions;
@@ -10,7 +10,7 @@ namespace foil
         static void Main(string[] args)
         {
             int[,,] finalM;
-            string dir = @"C:\Users\Catedras05\Desktop\TestTable.csv";
+            string dir = @"C:\Users\Lenovo\Desktop\TestTable.csv";
             string raw;
             using (var reader = new StreamReader(dir))
             {
@@ -27,8 +27,32 @@ namespace foil
             foreach (var Set in TestsRaw)
             {
                 var V = Set;
-                if (Set.Contains(",,")) { V = V.Substring(V.IndexOf(",,")+4); }
-                Coords.Add(Regex.Split(Set, "\r\n"));   
+                if (Set.Contains(",,")) { V = V.Substring(V.LastIndexOf(",,")+4); }
+                Coords.Add(Regex.Split(V, "\r\n"));   
+            }
+
+            //get the max amount of points
+            int Max=0;
+            foreach (var C in Coords)
+            {
+                if (Max<C.Length)
+                {
+                    Max = C.Length;
+                }
+            }
+
+            finalM = new int[TestsRaw.Length,Max,3];
+            var Buff = new String[3];
+
+            for (int i = 0; i < TestsRaw.Length; i++)
+            {
+                for (int i2 = 0; i2 < Coords[i].Length; i2++)
+                {
+                    Buff = Regex.Split(Coords[i][i2], ",");
+                    finalM[i, i2, 0] = Convert.ToInt32(Buff[0]);
+                    finalM[i, i2, 1] = Convert.ToInt32(Buff[1]);
+                    finalM[i, i2, 2] = Convert.ToInt32(Buff[2]);
+                }
             }
         }
     }
